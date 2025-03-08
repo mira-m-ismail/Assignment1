2e48ed519b5494da88e90279d2a9eccb61e0b723
srd8186 <srd8186@g.rit.edu>
2025-03-09 01:40:27 +0400

    Update grading_system.py

--- grading_system.py
+++ grading_system.py
@@ -14,6 +14,8 @@ def calculate_grade(score):
     elif score>=80:
         return 'B' 
     elif score>=70:
+        return 'C'
+    elif score >= 60:
         return 'D'
     else:
         return 'F'
